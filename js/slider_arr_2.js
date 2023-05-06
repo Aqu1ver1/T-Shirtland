7a0f5ac64e09917350f6f1666c44fa43e7826a35
Aqu1ver1 <maksim260107@gmail.com>
2023-05-07 02:56:09 +0300

    fixs

--- js/slider_arr_2.js
+++ js/slider_arr_2.js
@@ -5,22 +5,25 @@ const slider_wrapper_2 = document.querySelector('.wrapper_slider_arr_2'),
 
 let position = 0;
 
-btn_Prev_2.style.disabled = true
+btn_Prev_2.disabled = true
 btn_Next_2.addEventListener('click', function (){
 
   position = position - 360 * 3;
-  btn_Next_2.style.disabled = true
-  btn_Prev_2.style.disabled = false
+  btn_Next_2.disabled = true
+  btn_Prev_2.disabled = false
+  dots_2[0].classList.toggle('dots_style_active')
+  dots_2[1].classList.toggle('dots_style_active')
 
-  slider_wrapper_2.style.left = '-1080px';
+  slider_wrapper_2.style.left = position + 'px'
 })
 
-btn_Next_2.addEventListener('click', function (){
+btn_Prev_2.addEventListener('click', function (){
 
   position = position + 360 * 3;
-
-  btn_Next_2.style.disabled = false
-  btn_Prev_2.style.disabled = true
+  dots_2[0].classList.toggle('dots_style_active')
+  dots_2[1].classList.toggle('dots_style_active')
+  btn_Next_2.disabled = false
+  btn_Prev_2.disabled = true
 
   slider_wrapper_2.style.left = position +'px';
 })
